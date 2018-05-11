8be51ef4c1134bb0fe5f4929a9b84f34400307af
Ahmadooof <ahmad.a.93@hotmail.com>
2018-05-11 15:40:10 +0200

    updating navbar design

--- src/app/services/StudentService.ts
+++ src/app/services/StudentService.ts
@@ -10,7 +10,7 @@ const httpOptions = {
 @Injectable()
 export class StudentService {
     constructor(private http:HttpClient) {}
-    private url : string = 'https://hello-dot-schooldatabast.appspot.com/api/students';
+    private url : string = 'http://localhost:8080/hello/api/students';
 
     public getStudents() : Observable<Student[]> {
         return this.http.get<Student[]>(this.url);
